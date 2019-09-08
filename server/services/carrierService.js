09a145ab55d7bba689021d24ebeec475d1e06674
cbkm-company <you@example.com>
2019-09-09 01:18:20 +0530

    changes 8/89

--- server/services/carrierService.js
+++ server/services/carrierService.js
@@ -1,33 +1,48 @@
+
+var config = {
+    apiKey: "AIzaSyAdW_Gxfk0NseV7d0iGnumbtzU81urQcWQ",
+    authDomain: "jg-admin.firebaseapp.com",
+    databaseURL: "https://jg-admin.firebaseio.com",
+    projectId: "jg-admin",
+    storageBucket: "jg-admin.appspot.com",
+    messagingSenderId: "164457989225",
+    appId: "1:164457989225:web:ed31d9d175430711"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
+
 function submitCarrier() {
-    alert('Hello')
+    document.getElementById("submitload").innerHTML=`<div class="lds-dual-ring"></div>`
     name = document.getElementById('name').value;
     email = document.getElementById('email').value;
     contact = document.getElementById('contact').value;
     fileButton = document.getElementById('file');
-    file = fileButton.files[0];
-    alert(file)
-    var storageRef = firebase.storage().ref('JG/'+file.name);
-    storageRef.put(file);
-    alert('qwerty');
-  // addCarrier(name, email, contact, file.name)
+    var blob = file.files[0].slice(0, fileButton.files[0].size, 'image/png');
+    if (name && email && contact && fileButton) {
+        file = fileButton.files[0];
+        ext = file.name.split('.')[file.name.split('.').length - 1]
+        newFile = new File([blob], `${Date.now()}.${ext}`, { type: 'image/png' });
+        var storageRef = firebase.storage().ref('JG/' + newFile.name);
+        storageRef.put(newFile).then(function (snapshot) {
+            addCarrier(name, email, contact, newFile.name)
+            alert("Thank you for Submiting")
+            window.location.reload()
+        });
+        ;
+
+
+    }
+    else {
+        alert("Please enter all values")
+    }
 
 
 }
 
-
-function myFunction() {
-var fileButton = document.getElementById("fileButton");
-var file = fileButton.files[0];
-alert(file)
-alert('hello')
-var storageRef = firebase.storage().ref('JG/'+file.name);
-storageRef.put(file);
-alert('123')
-}
 
 
 function addCarrier(name, email, contact, fname) {
-    alert('doing')
+    
     return new Promise((resolve, reject) => {
 
         firebase.database().ref('admin/carrier/' + Date.now()).set({
@@ -36,7 +51,7 @@ function addCarrier(name, email, contact, fname) {
             contact: contact,
             fname: fname,
             date: new Date().toISOString(),
-            read:false
+            read: false
         })
             .then(() => resolve({ data: "enquiry Added", success: true }))
             .catch(() => reject({ data: "Error in adding enquiry", success: false }))
@@ -44,59 +59,59 @@ function addCarrier(name, email, contact, fname) {
 
 }
 
-function delt(file){
-// Create a reference to the file to delete
-var storageRef = firebase.storage().ref('JG');
+function delt(file) {
+    // Create a reference to the file to delete
+    var storageRef = firebase.storage().ref('JG');
 
-var desertRef = storageRef.child(file);
+    var desertRef = storageRef.child(file);
 
-// Delete the file
-desertRef.delete().then(function() {
-// File deleted successfully
-alert('success')
-}).catch(function(error) {
-// Uh-oh, an error occurred!
-});
-alert('1');
+    // Delete the file
+    desertRef.delete().then(function () {
+        // File deleted successfully
+        alert('success')
+    }).catch(function (error) {
+        // Uh-oh, an error occurred!
+    });
+    alert('1');
 
 }
 
-function download(file){
-var storageRef = firebase.storage().ref('JG');
+function download(file) {
+    var storageRef = firebase.storage().ref('JG');
 
-// Create a reference to the file we want to download
-var starsRef = storageRef.child(file);
+    // Create a reference to the file we want to download
+    var starsRef = storageRef.child(file);
 
-// Get the download URL
-starsRef.getDownloadURL().then(function(url) {
-console.log(url)
-// Insert url into an <img> tag to "download"
-}).catch(function(error) {
+    // Get the download URL
+    starsRef.getDownloadURL().then(function (url) {
+        console.log(url)
+        // Insert url into an <img> tag to "download"
+    }).catch(function (error) {
 
-// A full list of error codes is available at
-// https://firebase.google.com/docs/storage/web/handle-errors
-console.log(error.code)
-switch (error.code) {
-case 'storage/object-not-found':
-alert('1')
-// File doesn't exist
-break;
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        console.log(error.code)
+        switch (error.code) {
+            case 'storage/object-not-found':
+                alert('1')
+                // File doesn't exist
+                break;
 
-case 'storage/unauthorized':
-alert('2')
-// User doesn't have permission to access the object
-break;
+            case 'storage/unauthorized':
+                alert('2')
+                // User doesn't have permission to access the object
+                break;
 
-case 'storage/canceled':
-alert('3')
-// User canceled the upload
-break;
+            case 'storage/canceled':
+                alert('3')
+                // User canceled the upload
+                break;
 
-case 'storage/unknown':
-alert('4')
-// Unknown error occurred, inspect the server response
-break;
-}
-});
+            case 'storage/unknown':
+                alert('4')
+                // Unknown error occurred, inspect the server response
+                break;
+        }
+    });
 
 }
